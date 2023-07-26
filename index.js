fe9e8df24b48b1037da2d0b0fff5c7f885b87ef4
sushmita-singh <sushmitas326@gmail.com>
2023-07-26 15:33:39 +0530

    Body-parser added using middleware

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json());
+// // To implement malware 
+
+// function middleware1(req, res, next) {
+//     console.log("From inside middleware " + req.headers.counter);
+//     // If you're not going to call next() send error
+//     res.send("Error from inside Middleware");
+//     // next();
+// }
+
+// app.use(middleware1);
 
 function calculate(counter) {
     var sum = 0;
@@ -11,18 +24,30 @@ function calculate(counter) {
 }
 
 
-app.get('/handleSum', (req, res) => {
-    var counter = req.query.counter;
+// app.get('/handleSum', (req, res) => {
+//     var counter = req.query.counter;
+//     var name = req.query.name;
+//     var calculatedSum = calculate(counter);
+//     console.log(calculatedSum);
+//     var ans = "Hello "+ name + ", the sum is " + calculatedSum;
+//     res.send(ans);
+// })
+
+app.post('/createUser', (req,res) => {
+    res.send("User Created");
+
+})
+
+app.post('/handleSum', (req, res) => {
+    // var counter = req.query.counter;
+    // console.log(req.headers);
+    console.log(req.body)
+    var counter = req.headers.counter;
     var name = req.query.name;
     var calculatedSum = calculate(counter);
     console.log(calculatedSum);
     var ans = "Hello "+ name + ", the sum is " + calculatedSum;
     res.send(ans);
-  })
-
-app.post('/createUser', (req,res) => {
-    res.send("User Created");
-
 })
   
 app.listen(port, () => {
